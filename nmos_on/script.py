8262f98728974e18d28a3edd5f21369a62d59809
vishwateja12 <82596729+vishwateja12@users.noreply.github.com>
2024-03-14 21:19:52 +0530

    Add files via upload

---
+++ nmos_on/script.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from tabulate import tabulate
+
+file_name = 'W6nmos_ON_simulation_results.txt'
+
+with open(file_name, 'r') as file:
+    # Read data line by line and split each line
+    data = [line.split() for line in file]
+
+# Display the data in tabulated form
+headers = ["Temp", "M1[W]", "V-sweep", "V(drain)", "V(source)", "V(gate)", "V(body)", "I(Vd)", "I(Vg)", "I(Vs)", "I(Vb)"]
+table = tabulate(data,headers, tablefmt="fancy_grid")
+print(table)
+
+output_file = 'W6.txt'
+with open(output_file, 'w') as output:
+    print(table, file=output)
