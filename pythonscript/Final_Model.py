033801a886074c26ff71a6d3cceb9a2840922fd2
vishwateja12 <82596729+vishwateja12@users.noreply.github.com>
2024-03-27 16:52:08 +0530

    Add files via upload

--- pythonscript/Final_Model.py
+++ pythonscript/Final_Model.py
@@ -447,32 +447,51 @@ def AND(A0,A1,width,vdd):
 
     return leakage_sub,leakage_body,leakage_gate
 
-def OR(A0,A1,width,vdd):
-    leakage =0
-    if((A0==0) and (A1==0)):
-         leakage = 0
-    elif((A0==0) and (A1==1)):
-         leakage =0
-    elif((A0==1) and (A1==0)):
-         leakage =0
-    else:
-         leakage =0
+# def OR(A0,A1,width,vdd):
+#     leakage_sub =0
+#     leakage_body = 0
+#     leakage_gate = 0
+#     if((A0==0) and (A1==0)):
+#          leakage_sub = abs(ISubN(vn00[wToi(width)],width)) + abs(ISubP(vdd,width))  
+#          leakage_body = 2*(abs(IbodyN_off(vn00[wToi(width)],width))+abs(IbodyN_off(vdd,width)) ) + 2*abs(IbodyP_on(0.01,width))+ abs(IbodyN_on(vdd,width)) + abs(IbodyP_off(vdd,width))
+#          leakage_gate = 2*(abs(IgateN_off(vn00[wToi(width)],width))+abs(IgateN_off(vdd,width)) ) + 2*abs(IgateP_on(0.01,width))+ abs(IgateN_on(vdd,width)) + abs(IgateP_off(vdd,width))
+#     elif((A0==0) and (A1==1)):
+#          leakage_sub = abs(ISubN(vdd,width)) + abs(ISubP(vdd,width)) 
+#          leakage_body = abs(IbodyN_off(vdd,width))+abs(IbodyN_on(vdd,width))+abs(IbodyP_on(0.01,width))+ abs(IbodyN_on(vdd,width)) + 2*abs(IbodyP_off(vdd,width))
+#          leakage_gate = abs(IgateN_off(vdd,width))+abs(IgateN_on(vdd,width))+abs(IgateP_on(0.01,width))+ abs(IgateN_on(vdd,width)) + 2*abs(IgateP_off(vdd,width))
+#     elif((A0==1) and (A1==0)):
+#          leakage_sub =abs(ISubN(vn10[wToi(width)],width))+abs(ISubP(vdd,width)) 
+#          leakage_body = abs(IbodyN_off(vn10[wToi(width)],width))+abs(IbodyN_on(vdd-vn10[wToi(width)],width))+abs(IbodyP_on(0.01,width))+ abs(IbodyN_on(vdd,width)) + 2*abs(IbodyP_off(vdd,width))
+#          leakage_gate = abs(IgateN_off(vn10[wToi(width)],width))+abs(IgateN_on(0.01,width))+abs(IgateP_on(0.01,width))+ abs(IgateN_on(vdd,width)) + 2*abs(IgateP_off(vdd,width))
+#     else:
+#          leakage_sub =2*abs(ISubP(vdd,width))+abs(ISubN(vdd,width))
+#          leakage_body = 2*abs(IbodyN_on(vdd,width)) + 2*abs(IbodyP_off(vdd,width))+abs(IbodyP_on(0.01,width))+abs(IbodyN_off(vdd,width))
+#          leakage_gate = 2*abs(IgateN_on(vdd,width)) + 2*abs(IgateP_off(vdd,width))+abs(IgateP_on(0.01,width))+abs(IgateN_off(vdd,width))
 
-    return leakage
+#     return leakage_sub,leakage_body,leakage_gate
 
-def NOR(A0,A1,width,vdd):
-    leakage =0
-    if((A0==0) and (A1==0)):
-         leakage = 0
-    elif((A0==0) and (A1==1)):
-         leakage =0
-    elif((A0==1) and (A1==0)):
-         leakage =0
-    else:
-         leakage =0
+# def NOR(A0,A1,width,vdd):
+#     leakage_sub =0
+#     leakage_body = 0
+#     leakage_gate = 0
+#     if((A0==0) and (A1==0)):
+#          leakage_sub = abs(ISubN(vn00[wToi(width)],width)) + abs(ISubP(vdd,width))  
+#          leakage_body = 2*(abs(IbodyN_off(vn00[wToi(width)],width))+abs(IbodyN_off(vdd,width)) ) + 2*abs(IbodyP_on(0.01,width))+ abs(IbodyN_on(vdd,width)) + abs(IbodyP_off(vdd,width))
+#          leakage_gate = 2*(abs(IgateN_off(vn00[wToi(width)],width))+abs(IgateN_off(vdd,width)) ) + 2*abs(IgateP_on(0.01,width))+ abs(IgateN_on(vdd,width)) + abs(IgateP_off(vdd,width))
+#     elif((A0==0) and (A1==1)):
+#          leakage_sub = abs(ISubN(vdd,width)) + abs(ISubP(vdd,width)) 
+#          leakage_body = abs(IbodyN_off(vdd,width))+abs(IbodyN_on(vdd,width))+abs(IbodyP_on(0.01,width))+ abs(IbodyN_on(vdd,width)) + 2*abs(IbodyP_off(vdd,width))
+#          leakage_gate = abs(IgateN_off(vdd,width))+abs(IgateN_on(vdd,width))+abs(IgateP_on(0.01,width))+ abs(IgateN_on(vdd,width)) + 2*abs(IgateP_off(vdd,width))
+#     elif((A0==1) and (A1==0)):
+#          leakage_sub =abs(ISubN(vn10[wToi(width)],width))+abs(ISubP(vdd,width)) 
+#          leakage_body = abs(IbodyN_off(vn10[wToi(width)],width))+abs(IbodyN_on(vdd-vn10[wToi(width)],width))+abs(IbodyP_on(0.01,width))+ abs(IbodyN_on(vdd,width)) + 2*abs(IbodyP_off(vdd,width))
+#          leakage_gate = abs(IgateN_off(vn10[wToi(width)],width))+abs(IgateN_on(0.01,width))+abs(IgateP_on(0.01,width))+ abs(IgateN_on(vdd,width)) + 2*abs(IgateP_off(vdd,width))
+#     else:
+#          leakage_sub =2*abs(ISubP(vdd,width))+abs(ISubN(vdd,width))
+#          leakage_body = 2*abs(IbodyN_on(vdd,width)) + 2*abs(IbodyP_off(vdd,width))+abs(IbodyP_on(0.01,width))+abs(IbodyN_off(vdd,width))
+#          leakage_gate = 2*abs(IgateN_on(vdd,width)) + 2*abs(IgateP_off(vdd,width))+abs(IgateP_on(0.01,width))+abs(IgateN_off(vdd,width))
 
-    return leakage
-
+#     return leakage_sub,leakage_body,leakage_gate
 def NOT(A0,width,vdd):
     leakage_sub =0
     leakage_body = 0
@@ -488,15 +507,210 @@ def NOT(A0,width,vdd):
 
     return leakage_sub,leakage_body,leakage_gate
 
-leakage_sub,leakage_body,leakage_gate = NOT(0,8,0.8)
+
+
+def AND_use(A0,A1) :
+    leakage_sub =0
+    leakage_body = 0
+    leakage_gate = 0
+    if (A0==0) and (A1==0):
+        leakage_sub = 4.285e-09
+        leakage_body = 2.3314e-13
+        leakage_gate = 9.6402e-12
+    elif (A0==0) and (A1==1):
+        leakage_sub = 8.2374e-09
+        leakage_body = 4.128e-13
+        leakage_gate = 2.35e-11
+    elif (A0==1) and (A1==0):
+        leakage_sub = 5.698e-09
+        leakage_body = 4.28e-10
+        leakage_gate = 6.321e-11
+    else:
+        leakage_sub = 1.4823e-08
+        leakage_body = 3.489e-10
+        leakage_gate = 3.734e-11
+    return leakage_sub,leakage_body,leakage_gate
+
+def OR_use(A0,A1) :
+    leakage_sub =0
+    leakage_body = 0
+    leakage_gate = 0
+    if (A0==0) and (A1==0):
+        leakage_sub = 7.878e-09
+        leakage_body = 3.576e-12
+        leakage_gate = 9.959e-12
+    elif (A0==0) and (A1==1):
+        leakage_sub = 2.2206e-08
+        leakage_body = 4.126e-10
+        leakage_gate = 2.821e-11
+    elif (A0==1) and (A1==0):
+        leakage_sub = 7.256e-09
+        leakage_body = 4.6301e-13
+        leakage_gate = 5.026e-12
+    else:
+        leakage_sub = 1.941e-09
+        leakage_body = 5.708e-13
+        leakage_gate = 1.00525e-11
+    return leakage_sub,leakage_body,leakage_gate
+
+def NOR_use(A0,A1) :
+    leakage_sub =0
+    leakage_body = 0
+    leakage_gate = 0
+    if (A0==0) and (A1==0):
+        leakage_sub = 3.645e-09
+        leakage_body = 3.468e-12
+        leakage_gate = 9.959e-12
+    elif (A0==0) and (A1==1):
+        leakage_sub = 2.0107e-08
+        leakage_body = 8.954e-10
+        leakage_gate = 7.6202e-11
+    elif (A0==1) and (A1==0):
+        leakage_sub = 5.4274e-09
+        leakage_body = 1.0807e-13
+        leakage_gate = 5.026e-12
+    else:
+        leakage_sub = 1.1533e-10
+        leakage_body = 2.1552e-13
+        leakage_gate = 1.005e-11
+    return leakage_sub,leakage_body,leakage_gate
+
+def NOT_use(A0) :
+    leakage_sub =0
+    leakage_body = 0
+    leakage_gate = 0
+    if (A0==0) :
+        leakage_sub = 1.822e-09
+        leakage_body = 3.552e-13
+        leakage_gate = 3.604e-12
+    else:
+        leakage_sub = 4.2317e-09
+        leakage_body = 1.078e-13
+        leakage_gate = 5.026e-12
+    return leakage_sub,leakage_body,leakage_gate
+
+
+
+leakage_sub,leakage_body,leakage_gate = nstack(0,0,2,0.8)
 print(leakage_sub)
 print(leakage_body)
 print(leakage_gate)
 
+leakage_sub,leakage_body,leakage_gate = NOT(0,1,0.8)
+print(leakage_sub)
+print(leakage_body)
+print(leakage_gate)
 
-leakage_sub = abs(ISubN(vn00[5],8))
-leakage_body = 2*(abs(IbodyN_off(vn00[5],8))+abs(IbodyN_off(0.8,8)) )
-leakage_gate = 2*(abs(IgateN_off(vn00[5],8))+abs(IgateN_off(0.8,8)) )
+def Final_circuit(G0,G1,G2,G3,P0,P1,P2,P3,Cn) :
+    leakage_sub =0
+    leakage_body = 0
+    leakage_gate = 0
+
+    W1 = P0&G0
+    W2 = ~Cn
+    W3 = G0&W2
+    Cnx = (~W1)&(~W3)
+
+    W4 = P1&G1
+    W5 = G1&W1
+    W6 = W3&G1
+    W7 = W4|W5
+    Cny = (~W7)&(~W6)
+
+    W8 = P2&G2
+    W9 = W4&G2
+    W10 = G1&G2
+    W11 = W1&W10
+    W12 = W3&W10
+    W13 = W8|W9
+    W14 = W11|W12
+    Cnz = (~W13)&(W14)
+
+    W15 = P3&G3
+    W16 = W8&G3
+    W17 = P1&G3
+    W18 = W17&W10
+    W19 = G0&G3
+    W20 = W10&W19
+    W21 = W15|W16
+    W22 = W18|W20
+    G = W21|W22
+
+    W23 = P0|P1
+    W24 = P2|P3
+    P = W23|W24
+
+    leakage_sub += AND_use(P0,G0)[0]
+    leakage_sub += NOT_use(Cn)[0]
+    leakage_sub += AND_use(G0,W2)[0]
+    leakage_sub += NOR_use(W1,W3)[0]
+
+    leakage_sub += AND_use(P1,G1)[0]
+    leakage_sub += AND_use(G1,W1)[0]
+    leakage_sub += AND_use(W3,G1)[0]
+    leakage_sub += OR_use(W4,W5)[0]
+    leakage_sub += NOR_use(W7,W6)[0]
+
+    leakage_sub += AND_use(P2,G2)[0]
+    leakage_sub += AND_use(W4,G2)[0]
+    leakage_sub += AND_use(G1,G2)[0]
+    leakage_sub += AND_use(W1,W10)[0]
+    leakage_sub += AND_use(W3,W10)[0]
+    leakage_sub += OR_use(W8,W9)[0]
+    leakage_sub += OR_use(W11,W12)[0]
+    leakage_sub += NOR_use(W13,W14)[0]
+
+    leakage_sub += AND_use(P3,G3)[0]
+    leakage_sub += AND_use(W8,G3)[0]
+    leakage_sub += AND_use(P1,G3)[0]
+    leakage_sub += AND_use(W17,W10)[0]
+    leakage_sub += AND_use(G0,G3)[0]
+    leakage_sub += AND_use(W10,W19)[0]
+    leakage_sub += OR_use(W15,W16)[0]
+    leakage_sub += OR_use(W18,W20)[0]
+    leakage_sub += OR_use(W21,W22)[0]
+    
+    leakage_sub += OR_use(P0,P1)[0]
+    leakage_sub += OR_use(P2,P3)[0]
+    leakage_sub += OR_use(W23,W24)[0]
+
+    leakage_body += AND_use(P0,G0)[1]
+    leakage_body += NOT_use(Cn)[1]
+    leakage_body += AND_use(G0,W2)[1]
+    leakage_body += NOR_use(W1,W3)[1]
+
+    leakage_body += AND_use(P1,G1)[1]
+    leakage_body += AND_use(G1,W1)[1]
+    leakage_body += AND_use(W3,G1)[1]
+    leakage_body += OR_use(W4,W5)[1]
+    leakage_body += NOR_use(W7,W6)[1]
+
+    leakage_body += AND_use(P2,G2)[1]
+    leakage_body += AND_use(W4,G2)[1]
+    leakage_body += AND_use(G1,G2)[1]
+    leakage_body += AND_use(W1,W10)[1]
+    leakage_body += AND_use(W3,W10)[1]
+    leakage_body += OR_use(W8,W9)[1]
+    leakage_body += OR_use(W11,W12)[1]
+    leakage_body += NOR_use(W13,W14)[1]
+
+    leakage_body += AND_use(P3,G3)[1]
+    leakage_body += AND_use(W8,G3)[1]
+    leakage_body += AND_use(P1,G3)[1]
+    leakage_body += AND_use(W17,W10)[1]
+    leakage_body += AND_use(G0,G3)[1]
+    leakage_body += AND_use(W10,W19)[1]
+    leakage_body += OR_use(W15,W16)[1]
+    leakage_body += OR_use(W18,W20)[1]
+    leakage_body += OR_use(W21,W22)[1]
+    
+    leakage_body += OR_use(P0,P1)[1]
+    leakage_body += OR_use(P2,P3)[1]
+    leakage_body += OR_use(W23,W24)[1]
 
 
+    return P,G,Cnx,Cny,Cnz,leakage_sub,leakage_body,leakage_gate
 
+leakage_sub = Final_circuit(0,0,0,0,0,0,0,0,0)[5]
+leakage_body = Final_circuit(0,0,0,0,0,0,0,0,0)[6]
+print(leakage_sub,leakage_body)
